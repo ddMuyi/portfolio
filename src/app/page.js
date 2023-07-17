613e2f14695c2cf34edf69a6c5baf5d160409628
david660 <daodu.muyiwa@gmail.com>
2023-07-17 16:56:43 +0100

    Netlify bug

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,13 @@ export default function Home() {
   return (
     <article className="space-y-4 text-[#f8f8f8] backdrop-blur-md">
       <p className="leading-relaxed">
-        A microbiologist turned <span className="text-blue-400">web developer</span>. A friend introduced software development to me shortly before graduation, but I had little learning time to spare. I learnt the basics of Java, although, I was able to learn about OOP,I had little knowledge about Java's implementation in the real-world.
+        A microbiologist turned <span className="text-blue-400">web developer</span>. A friend introduced software development to me shortly before graduation, but I had little learning time to spare. I learnt the basics of Java, although, I was able to learn about OOP, I had little knowledge about Java's implementation in the real-world.
       </p>
       <p className="leading-relaxed">
         Some after graduation, I gained interest in Tech once more, and this time I started learning about the basics of Web development (HTML and CSS) and Javascript, and the rest is history.
       </p>
       <p className="leading-relaxed">
-        I&apos;ve been able to work on Enterprise applications and Basic web pages across several sectors which includes <span className="text-blue-400">e-commerce, healthTech, e-Learning and finTech</span>.
+        I have been able to work on Enterprise applications and Basic web pages across several sectors which includes <span className="text-blue-400">e-commerce, healthTech, e-Learning and finTech</span>.
       </p>
     </article>
   )
